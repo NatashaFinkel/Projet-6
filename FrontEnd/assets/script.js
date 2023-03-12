eeb48a27ce6a6def8b678055c1d4cc4ffcec67a3
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2023-03-12 11:28:54 +0100

    Création des filtres (pas terminés)

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,6 +1,7 @@
 //  Création de variables vides (pour l'instant !).
 let categories;
 let works;
+let categoriesData;
 
 //  Pour ne pas avoir à réécrire la même chose
 //  si on doit télécharger d'autres données de la
@@ -25,28 +26,11 @@ async function fetchWorkData() {
 fetchWorkData().then((workData) => {
   console.log("Les travaux précédents ont été transférés !");
 
-  //  Les données sont placées dans la variable works qui
-  //  devient un tableau.
+  //  Les données sont placées dans la variable works
   const works = workData;
 
   worksGenerator(works);
-});
-
-//  Pour récupérer les données des
-//  catégories déjà existantes.
-async function fetchCategories() {
-  const response = await fetch(`${apiUrl}/categories`);
-  console.log("Le Fetch (catégories) a réussi !");
-
-  const categoriesData = await response.json();
-  console.log(categoriesData);
-  return categoriesData;
-}
-
-fetchCategories().then((categoriesData) => {
-  console.log("Les catégories ont été transférées !");
-
-  categories = categoriesData;
+  filterCreator();
 });
 
 function worksGenerator(works) {
@@ -73,5 +57,95 @@ function worksGenerator(works) {
     createFigure.appendChild(createImage);
     createFigure.appendChild(createFigCaption);
   }
-  console.log();
 }
+
+
+//  Pour récupérer les données des
+//  catégories déjà existantes.
+async function fetchCategories() {
+  const response = await fetch(`${apiUrl}/categories`);
+  console.log("Le Fetch (catégories) a réussi !");
+
+  const data = await response.json();
+  console.log(data);
+
+  categoriesData = data;
+  return categoriesData;
+}
+
+fetchCategories().then((categoriesData) => {
+  console.log("Les catégories ont été transférées !");
+  return categoriesData;
+});
+
+function filterCreator() {
+  let filter = document.getElementById("filter-btn");
+  // let nodeList = document.querySelectorAll("input");
+
+  for (let i = 0; i < 4; i++) {
+    const filterBtn = document.createElement("input");
+    filterBtn.setAttribute("type", "submit");
+    filterBtn.setAttribute("value", "");
+
+
+    //  Pour placer les éléments crées en haut 
+    //  du container (gallery)
+   filter.prepend(filterBtn);
+
+  }
+}
+
+//  on prend le nombre d'éléments inputs crées
+//   dans cette fonction comme index i.
+//  i = nodeList[0];
+// console.log(nodeList.length);
+//  console.log(nodeList[0]);
+
+//  let nodeList = document.querySelectorAll("input");
+//  console.log(nodeList);
+
+// for (let i = 0; i < nodeList.length; i++) {
+// nodeList[i].setAttribute("value", " ");
+
+///////  ici ça marche !
+// Pour lister les inputs et les afficher dans la console.
+//   let nodeList = document.querySelectorAll("input");
+//   console.log(nodeList);
+
+/* function Filtername() {
+  let nodeList = document.querySelectorAll("input");
+  console.log(nodeList);
+
+ // for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].setAttribute("value", " ");
+  } */
+//  console.log(nodeList);
+//    console.log(nodeList[0].value);
+
+//  for (let i = 0; i < nodeList.length; i++) {
+//   nodeList[0].setAttribute("value", categoriesData[0].name);
+
+////// ça marche !
+//    nodeList[1].setAttribute("value", "vanille");
+
+//    nodeList[0].setAttribute("value", categoriesData[i].name);
+// }
+
+// let nodeList = document.querySelectorAll("input");
+//  console.log(nodeList);
+
+//  for (let i = 0; i < 4; i++) {
+
+//}
+//}
+//console.log(nodeList);
+//   const nodeList = document.querySelectorAll("input");
+//   nodeList[0].setAttribute("value", "Tous");
+//  nodeList[1].setAttribute("value", categories[1].name);
+
+// Pour lister les inputs et les afficher dans la console.
+// const nodeList = document.querySelectorAll("input");
+// console.log(nodeList);
+
+//  nodeList[0].setAttribute("value", "Tous");
+// nodeList[1].setAttribute("value", categories[1].name);
