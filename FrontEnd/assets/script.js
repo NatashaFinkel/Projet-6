676277a765178a24b8604ecc1c72b341bca00314
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2023-03-14 14:52:17 +0100

    Essai fonction pour les filtres
    (pas fini !)

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -2,6 +2,9 @@
 let categories;
 let works;
 let categoriesData;
+let categoriesId;
+let worksId;
+let worksContent;
 
 //  Pour ne pas avoir à réécrire la même chose
 //  si on doit télécharger d'autres données de la
@@ -28,6 +31,7 @@ fetchWorkData().then((workData) => {
 
   //  Les données sont placées dans la variable works
   const works = workData;
+//  console.log(workData);
 
   worksGenerator(works);
 });
@@ -40,6 +44,12 @@ function worksGenerator(works) {
   //  La boucle for va créer le nouveau contenu de gallery.
   for (let i = 0; i < works.length; i++) {
     const newGallery = works[i];
+
+//  const worksContent = works[i];
+//  console.log(worksContent);
+
+ // const worksId = categoriesContent.id;
+//  console.log(worksId);
 
     const galleryNewLocation = document.querySelector(".gallery");
 
@@ -57,7 +67,7 @@ function worksGenerator(works) {
     const createFigCaption = document.createElement("figcaption");
     createFigCaption.innerHTML = newGallery.title;
 
-    //  appendChild sert à placer les éléments 
+    //  appendChild sert à placer les éléments
     //  en dernier dans galleryNewLocation (= la class gallery).
     galleryNewLocation.appendChild(createFigure);
     createFigure.appendChild(createImage);
@@ -78,10 +88,12 @@ async function fetchCategories() {
   return categoriesData;
 }
 
+
 fetchCategories().then((categoriesData) => {
   console.log("Les catégories ont été transférées !");
 
   const categories = categoriesData;
+  console.log(categories);
 
   categoriesGenerator(categories);
 });
@@ -90,29 +102,80 @@ function categoriesGenerator(categories) {
   console.log(categories);
 
   for (let i = 0; i < categories.length; i++) {
-    //  La variable WW représente l'index i (chacun des éléments)
+    //  La variable categoriesContent représente l'index i (chacun des éléments)
     //   de l'array categories.
-    const WW = categories[i];
+    const categoriesContent = categories[i];
+    //  ici : inclure works / workData
 
     const filterLocation = document.querySelector(".filter-box");
 
     const filterButtons = document.createElement("buttons");
     filterButtons.setAttribute("class", "filter-btn");
-    filterButtons.innerHTML = WW.name;
+    filterButtons.innerHTML = categoriesContent.name;
 
     filterLocation.appendChild(filterButtons);
+
+    const categoriesId = categoriesContent.id;
+    console.log(categoriesId);
+
+
+    //  categories est le tableau avec les trois éléments filtres
+    //  console.log(categories);
+
+    //  categoriesContent est chaque élément filtre du tableau categories
+    //  console.log(categoriesContent);
   }
 
+
   //  Pour créer le premier filtre (qui est différent).
-  const filterLocation = document.querySelector(".filter-box");
+  const firstFilterLocation = document.querySelector(".filter-box");
   const firstFilterButton = document.createElement("button");
   firstFilterButton.setAttribute("class", "first-filter-btn");
   firstFilterButton.innerText = "Tous";
 
   //  Pour placer le firstFilterButton
   //  en premier dans filterLocation (= la class filter-box).
-  filterLocation.prepend(firstFilterButton);
+  firstFilterLocation.prepend(firstFilterButton);
 }
+
+//  Création d'un Set : pareil qu'un tableau, mais qui
+//  interdit les doublons.
+const categorySet = new Set();
+
+//  On ajoute les données du résultat des fonctions
+//  fetchCategories et fetchWorkData à categorySet.
+categorySet.add(fetchCategories());
+categorySet.add(fetchWorkData());
+
+console.log(categorySet);
+
+function filteredCategories(categoriesId, worksId) {
+
+  
+
+}
+
+//const categoryById = categorySet;
+//console.log(categoryById);
+////const filteredCategories = categoryById.filter(ZZ);
+
+//////function ZZ(categorySet) {
+
+///  return
+//categoryById   =
+
+// categorySet;
+//  console.log(categorySet);
+
+/////}
+
+// ici : mettre fonction filtre
+
+//const categoryById = categories.
+
+// const filteredData = categories.filter((category) => {
+//  return (selon categoryById)
+//}
 
 /////  Brouillons (on verra demain si on utilise ou pas !)
 
@@ -124,10 +187,6 @@ function categoriesGenerator(categories) {
 //  Pour ajouter le résultat de fetchCategories dans mySet
 //  mySet.add(fetchCategories());
 // console.log(mySet);
-
-// for (let i = 0; i < mySet.length; i++) {
-//  const filterButtons = document.createElement("buttons");
-//  filterSet.setAttribute("type", "submit");
 
 // mySet.add(filterButtons);
 //   filterLocation.add(mySet);
